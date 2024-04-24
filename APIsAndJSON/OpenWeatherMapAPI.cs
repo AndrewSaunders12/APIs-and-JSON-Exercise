c2241b4cd289157708ec6c1dcf3e652f4c63f2bc
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-04-24 12:19:38 -0400

    Ron an temp

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,35 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+        public static void WeatherMapAPI()
+        {
+            var client = new HttpClient();
+
+            var key = "a928eb068983835c87885cbe7fa6612b";
+            
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a city name for the tempture");
+                var cityName = Console.ReadLine();
+                Console.WriteLine();
+
+                var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={key}&units=imperial";
+                var response = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                var temp = JObject.Parse(formattedResponse).GetValue("temp");
+
+                Console.WriteLine($"The current tempture is {temp} degrees Fahrenheit");
+            }
+
+
+        }
+
+
+
+
+
     }
 }
